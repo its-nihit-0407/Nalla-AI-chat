8b8500d835dff276ab37b3ccce20c69094e042f2
Nishidh <nishidhsingh97@gmail.com>
2025-06-09 21:46:15 +0530

    Added Clerk Authetication

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,5 @@
-// export const GEMINI_API_KEY = import.meta.env.VITE_GOOGLE_GENERATIVE_AI_API;
-// export const CLERK_PUB_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-
-export const GEMINI_API_KEY = "AIzaSyBgSti-py2TwjlcvohjQKXTiPEjNbLMs30";
-export const CLERK_PUB_KEY = "pk_test_ZWFzeS1tYWtvLTUzLmNsZXJrLmFjY291bnRzLmRldiQ";
+export const GEMINI_API_KEY = import.meta.env.VITE_GOOGLE_GENERATIVE_AI_API;
+export const CLERK_PUB_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 export const generationConfig = {
   temperature: 1.85,
